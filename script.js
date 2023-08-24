3cefbe1f1c1156d4caf7c5e630a153c3e6ac5fc6
Andrei Scafi <andreiscafi@hotmail.com>
2023-08-24 15:36:16 -0300

    Make the dark mode work with Javascript and CSS.

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggle = document.querySelector('.toggle');
+const container = document.querySelector('.container')
+
+toggle.addEventListener('click', () => {
+    container.classList.toggle('dark') ? (toggle.firstElementChild.className = "far fa-moon") : (toggle.firstElementChild.className = "far fa-sun");
+});
